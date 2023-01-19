2a810ecf557c6c0b63eeedd54efc7803897ed711
ahmedaj <ahlinux@gmail.com>
2023-01-19 12:45:00 +0300

    cleaning

--- main.go
+++ main.go
@@ -11,12 +11,6 @@ import (
 
 //docker run -v ~/work:/work -p 8080:8080 -p 8090:8090 --name godev -i -t ubuntu:latest
 // album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
 
 func getIndexPage(c *gin.Context) {
 	c.Header("content-type", "text/html")
